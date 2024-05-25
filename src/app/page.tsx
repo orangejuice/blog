572fbee7b7b540c9f495b5f8f200cc9a8c228bb8
orangejuice <cherrymeteor@outlook.com>
2024-05-25 20:54:07 +0100

    init configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/logo.svg"
           alt="Next.js logo"
           width={180}
           height={38}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,9 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss"
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
