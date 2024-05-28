b525b8f73e916906feb7edc04a69a6ae8ca77605
orangejuice <cherrymeteor@outlook.com>
2024-05-28 21:08:27 +0100

    animate post

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -9,10 +9,11 @@ import {useMDXComponent} from "next-contentlayer2/hooks"
 import Toc from "@/components/toc"
 import {ReactionsButtons} from "@/components/reactions"
 import {Comments} from "@/components/comments"
-import {formatDate} from "@/lib/utils"
+import {formatDate, useCssIndexCounter} from "@/lib/utils"
 
 export async function generateMetadata({params}: {params: {slug: string}}): Promise<Metadata | undefined> {
   const slug = decodeURI(params.slug)
+
   const post = allPosts.find((post) => post.slug === slug)
   if (!post) return
 
@@ -55,26 +56,27 @@ export default async function Page({params}: {params: {slug: string}}) {
   const post = posts[postIndex]
   const prev = posts[postIndex - 1]
   const next = posts[postIndex + 1]
+  const cssIndexCounter = useCssIndexCounter()
 
   return <>
     <div className="grid md:grid-cols-[2fr,1fr] items-start gap-16 min-h-screen">
       <article className="overflow-auto">
         <div className="flex flex-col gap-3">
-          <h1 className="text-2xl font-bold">{post.title}</h1>
-          <p className="text-secondary">
+          <h1 className="text-2xl font-bold animate-delay-in" style={cssIndexCounter()}>{post.title}</h1>
+          <p className="text-secondary animate-delay-in" style={cssIndexCounter()}>
             <time dateTime={post.date}>{formatDate(post.date)}</time>
             {post.updated ? ` (Updated ${formatDate(post.updated)})` : ""}{" "}
           </p>
         </div>
         <div className="h-8"/>
-        <div className="prose toc-content max-w-none dark:prose-invert">
+        <div className="prose toc-content max-w-none dark:prose-invert animate-delay-in" style={cssIndexCounter()}>
           <MDXContent/>
         </div>
         <div className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300" id="comment">
           <Comments/>
         </div>
       </article>
-      <aside className="flex flex-col sticky top-8 gap-4">
+      <aside className="flex flex-col sticky top-8 gap-4 animate-delay-in" style={cssIndexCounter()}>
         <Toc/>
         <hr/>
         <div className="flex flex-col gap-2 dark:bg-gray-800">
