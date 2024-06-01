6c72cd3952f256dd6dd5c05647854d954983fb7c
orangejuice <cherrymeteor@outlook.com>
2024-06-01 17:00:16 +0100

    improves locale switcher style

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -28,11 +28,11 @@ export function LocaleSwitcher() {
       <p className="sr-only">{t(i18n.language)}</p>
       <Icons.nav.lang className="z-10 pointer-events-none"/>
       <select className={cn(buttonVariants({variant: "ghost", size: "icon"}),
-        "appearance-none pl-8 pr-1 -ml-7 cursor-pointer select-none",
-        "h-fit w-fit gap-2 whitespace-nowrap rounded-lg py-1.5 transition-all",
+        "appearance-none pl-8 pr-3 -ml-7 cursor-pointer select-none text-center",
+        "h-fit w-fit whitespace-nowrap rounded-lg py-1.5 transition-all",
         "hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
       )} value={i18n.language} disabled={isPending} onChange={onSelectChange}>
-        {site.locales.map((cur) => <option key={cur} value={cur}>{t(cur)}</option>)}
+        {site.locales.map((cur) => <option key={cur} value={cur}>{t(cur, {lng: cur})}</option>)}
       </select>
     </label>
   )
