2ef3bcb3a9b0836b038ac53727c680ef7a3d4552
orangejuice <cherrymeteor@outlook.com>
2024-06-10 15:45:41 +0100

    code cleanup

--- src/components/bgm-toggle.tsx
+++ src/components/bgm-toggle.tsx
@@ -6,8 +6,10 @@ import {cn} from "@/lib/utils"
 import {useTranslation} from "react-i18next"
 import React, {useEffect, useRef, useState} from "react"
 import {AnimatePresence, motion} from "framer-motion"
+import {useMounted} from "@/lib/hooks"
 
 export function BackgroundMusicToggle() {
+  const mounted = useMounted()
   const {resolvedTheme} = useTheme()
   const {t} = useTranslation()
   const [isWhiteNoiseOn, setWhiteNoiseOn] = useState<boolean>(false)
@@ -19,17 +21,21 @@ export function BackgroundMusicToggle() {
     isWhiteNoiseOn ? void audioRef.current.play().catch(console.log) : audioRef.current.pause()
   }, [isWhiteNoiseOn, resolvedTheme])
 
+  if (!mounted) return <Button variant="ghost" size="icon" className="h-4 w-4 px-2 py-1.5"/>
+
   return (<>
     <Button variant="ghost" size="icon" className={cn("group relative h-fit w-fit gap-2 px-2 py-1.5 -mx-2 hover:bg-stone-200 active:bg-stone-300",
       "dark:hover:bg-stone-700 dark:active:bg-stone-800")}
       onClick={() => setWhiteNoiseOn(!isWhiteNoiseOn)}>
       <Icons.audio className={cn(!isWhiteNoiseOn && "[&_path]:[animation-play-state:paused]")}/>
       <AnimatePresence>
-        {!isWhiteNoiseOn && <motion.span key="span" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}
-          className={cn("absolute w-6 h-1 rotate-45 top-4 left-2 bg-red-500 rounded",
-            "border border-white dark:border-black group-hover:border-stone-200 dark:group-hover:border-stone-700 transition-colors duration-700")}/>}
-        <span>{t("theme.white-noise")}</span>
+        {!isWhiteNoiseOn && (<>
+          <motion.span initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className={cn(
+            "absolute w-6 h-1 rotate-45 top-4 left-2 bg-red-500 rounded border border-white",
+            "dark:border-black group-hover:border-stone-200 dark:group-hover:border-stone-700 transition-colors duration-700")}/>
+        </>)}
       </AnimatePresence>
+      <span>{t("theme.white-noise")}</span>
     </Button>
     <audio ref={audioRef} className="hidden" loop={true}/>
   </>)
