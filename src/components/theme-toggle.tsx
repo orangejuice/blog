0494586766048d7c40eadc3bf61a72f1eb573237
orangejuice <cherrymeteor@outlook.com>
2024-06-11 12:31:33 +0100

    update icon tooltip logic

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -14,25 +14,27 @@ export function ThemeToggle() {
 
   if (!mounted) return <Button variant="ghost" size="icon" className="h-5 w-5 px-2 py-1.5"/>
 
-  return (
-    <Tooltip delayDuration={0}>
-      <TooltipTrigger asChild>
-        <Button variant="ghost" size="icon" className={cn("group h-fit w-fit gap-2 px-2 py-1.5 -mx-2 hover:bg-stone-200 active:bg-stone-300",
-          "dark:hover:bg-stone-700 dark:active:bg-stone-800")}
-          onClick={() => setTheme(theme === "light" ? "dark" : theme === "dark" ? "system" : "light")}>
-          {theme === "dark" && <Icons.theme.dark/>}
-          {theme === "light" && <Icons.theme.light/>}
-          {theme === "system" && <div className="relative h-5 w-5">
-            <Icons.theme.system className="absolute animate-delay-hide group-hover:animate-none"/>
-            {resolvedTheme === "dark" &&
-              <Icons.theme.dark className="absolute animate-delay-show opacity-0 group-hover:hidden"/>}
-            {resolvedTheme === "light" &&
-              <Icons.theme.light className="absolute animate-delay-show opacity-0 group-hover:hidden"/>}
-          </div>}
-          <span>{t(`theme.${resolvedTheme}`)}</span>
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent>{t(`theme.${theme}`)}</TooltipContent>
-    </Tooltip>
-  )
+  return (<>
+    <Button variant="ghost" size="icon" className={cn("group h-fit w-fit gap-2 px-2 py-1.5 -mx-2 hover:bg-stone-200 active:bg-stone-300",
+      "dark:hover:bg-stone-700 dark:active:bg-stone-800")}
+      onClick={() => setTheme(theme === "light" ? "dark" : theme === "dark" ? "system" : "light")}>
+      {theme === "dark" && <Icons.theme.dark/>}
+      {theme === "light" && <Icons.theme.light/>}
+      {theme === "system" && (<>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <div className="relative h-5 w-5">
+              <Icons.theme.system className="absolute animate-delay-hide group-hover:animate-none"/>
+              {resolvedTheme === "dark" &&
+                <Icons.theme.dark className="absolute animate-delay-show opacity-0 group-hover:hidden"/>}
+              {resolvedTheme === "light" &&
+                <Icons.theme.light className="absolute animate-delay-show opacity-0 group-hover:hidden"/>}
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>{t(`theme.${theme}`)}</TooltipContent>
+        </Tooltip>
+      </>)}
+      <span>{t(`theme.${resolvedTheme}`)}</span>
+    </Button>
+  </>)
 }
