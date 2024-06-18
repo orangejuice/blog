f2e016fa634c578ceae1e8eed8cfa749ae69a7f5
orangejuice <cherrymeteor@outlook.com>
2024-06-18 23:24:07 +0100

    improve button a11y

--- src/components/lang-select.tsx
+++ src/components/lang-select.tsx
@@ -1,13 +1,13 @@
 "use client"
 import {useLocalStorage} from "@/lib/use-local-storage"
-import Link from "next/link"
 import {cn} from "@/lib/utils"
 import {Icons} from "@/components/icons"
 import React, {ComponentPropsWithoutRef} from "react"
 import {useTranslation} from "react-i18next"
 import {useMounted} from "@/lib/hooks"
+import {Button} from "@/components/ui/button"
 
-export function LangSelect(props: ComponentPropsWithoutRef<"div">) {
+export function LangSelect(props: ComponentPropsWithoutRef<"button">) {
   const [lang, setLang] = useLocalStorage<"one" | "all">("latest-lang", "one")
   const {t, i18n: {language: locale}} = useTranslation("lang")
 
@@ -15,21 +15,22 @@ export function LangSelect(props: ComponentPropsWithoutRef<"div">) {
   if (!mounted) return null
 
   return (<>
-    <div {...props} className="flex items-center gap-1 animate-delay-in">
+    <Button variant="noStyle" onClick={() => setLang(prev => prev == "one" ? "all" : "one")}
+      {...props} className="p-0 h-fit gap-1 animate-delay-in group">
       <Icons.filter/>
-      <Link href="" onClick={() => setLang("one")}
-        className={cn("tracking-tight text-sm transition-all text-stone-600 underline-fade",
-          "dark:text-stone-400 dark:hover:text-stone-300 dark:active:text-stone-200",
-          lang != "all" ? "font-semibold" : "text-xs")}>
+      <span className={cn("tracking-tight text-sm transition-all text-stone-600",
+        "dark:text-stone-400 dark:hover:text-stone-300 dark:active:text-stone-200",
+        lang == "all" && "underline-fade underline-fade-with-group",
+        lang == "one" ? "font-semibold" : "text-xs")}>
         {t(locale)}
-      </Link>
+      </span>
       <Icons.symbol.slash className="-rotate-[30deg] w-3 h-3 -mx-1 stroke-2"/>
-      <Link href="" onClick={() => setLang("all")}
-        className={cn("tracking-tight text-sm transition-all text-stone-600 underline-fade",
-          "dark:text-stone-400 dark:hover:text-stone-300 dark:active:text-stone-200",
-          lang == "all" ? "font-semibold" : "text-xs")}>
+      <span className={cn("tracking-tight text-sm transition-all text-stone-600",
+        "dark:text-stone-400 dark:hover:text-stone-300 dark:active:text-stone-200",
+        lang == "one" && "underline-fade underline-fade-with-group",
+        lang == "all" ? "font-semibold" : "text-xs")}>
         {t("all")}
-      </Link>
-    </div>
+      </span>
+    </Button>
   </>)
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import {Slot} from "@radix-ui/react-slot"
+import {cva, type VariantProps} from "class-variance-authority"
 
-import { cn } from "@/lib/utils"
+import {cn} from "@/lib/utils"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-stone-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-stone-950 dark:focus-visible:ring-stone-800",
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-stone-100 text-stone-900 hover:bg-stone-100/80 dark:bg-stone-800 dark:text-stone-50 dark:hover:bg-stone-800/80",
         ghost: "hover:bg-stone-100 hover:text-stone-900 dark:hover:bg-stone-800 dark:hover:text-stone-50",
         link: "text-stone-900 underline-offset-4 hover:underline dark:text-stone-50",
+        noStyle: "",
       },
       size: {
         default: "h-10 px-4 py-2",
