a8f12882ad5cc3dec60ad386e7aff7beab7e007a
orangejuice <cherrymeteor@outlook.com>
2024-06-20 10:26:30 +0100

    seo - add rel icon

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound(props: {params: {locale: SiteLocale}}) {
   const {t} = useTranslation()
 
   return (<>
-    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
+    <div className="flex flex-col items-start justify-start grow md:flex-row md:items-center md:justify-center md:space-x-6">
       <div className="space-x-2 pb-8 pt-6 md:space-y-5">
         <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
           404
