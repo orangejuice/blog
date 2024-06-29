c5e4cad1fd00a1cab41dd58a05308b1154ed7b12
orangejuice <cherrymeteor@outlook.com>
2024-06-29 23:03:15 +0100

    use turbo

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,15 +2,24 @@ import type {i18n, Resource} from "i18next"
 import {createInstance} from "i18next"
 import resourcesToBackend from "i18next-resources-to-backend"
 import {initReactI18next} from "react-i18next/initReactI18next"
-import {site} from "@/site"
+import {site, SiteLocale} from "@/site"
+import enLang from "@/locales/en/lang.json"
+import enTrans from "@/locales/en/translation.json"
+import zhLang from "@/locales/zh/lang.json"
+import zhTrans from "@/locales/zh/translation.json"
+
+const localeResources = {
+  en: {lang: enLang, translation: enTrans}, zh: {lang: zhLang, translation: zhTrans}
+}
 
 export default async function initTranslation(locale: string, i18nInstance?: i18n, resources?: Resource) {
   i18nInstance ??= createInstance()
   i18nInstance.use(initReactI18next)
 
   if (!resources) {
-    i18nInstance.use(resourcesToBackend((language: string, namespace: string) =>
-      import(`@/locales/${language}/${namespace}.json`)))
+    i18nInstance.use(resourcesToBackend((language: SiteLocale, namespace: string) =>
+      // @ts-ignore
+      localeResources[language][namespace]))
   }
 
   await i18nInstance.init({
